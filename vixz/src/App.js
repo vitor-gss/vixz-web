99833603c13fe5ec4c6a1dd37ec9d7f71652ab5b
Vitor Gabriel da Silva Santos <126255280+vitor-gss@users.noreply.github.com>
2024-11-28 11:36:50 -0300

    abanana

--- vixz/src/App.js
+++ vixz/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { About } from './components/main/about';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <About/>
     </div>
   );
 }
---
+++ vixz/src/components/main/about.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <section class="about section-padding prelative" data-scroll-index='1'>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="sectioner-header text-center">
+              <h3>Sobre</h3>
+              <span class="line"></span>
+              <p>Um aplicativo de busca de vagas de emprego e cursos. Onde você expõe seu
+                currículo para as empresas, e ainda com cursos disponíveis para melhorar suas chances de
+                contratação!</p>
+            </div>
+            <div class="section-content text-center">
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="icon-box wow fadeInUp" data-wow-delay="0.2s"> <i class="fa fa-life-ring"
+                      aria-hidden="true"></i>
+                    <h5>Vagas de emprego</h5>
+                    <p>Tenha acesso a diversas vagas de emprego em um único lugar.
+                    </p>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="icon-box wow fadeInUp" data-wow-delay="0.4s"> <i class="fa fa-mobile"
+                      aria-hidden="true"></i>
+                    <h5>Cursos</h5>
+                    <p>Diversos cursos disponíveis para você adquirir ou aprimorar suas habilidades.
+                    </p>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="icon-box wow fadeInUp" data-wow-delay="0.6s"> <i class="fa fa-bolt"
+                      aria-hidden="true"></i>
+                    <h5>Fóruns</h5>
+                    <p>Alguma dúvida? Acesse um de nossos fóruns.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <a href="#" class="about-btn">Veja mais!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
