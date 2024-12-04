c408cec937e1bb1e8c88df2f01084498d02b27ce
Vitor Gabriel da Silva Santos <126255280+vitor-gss@users.noreply.github.com>
2024-12-04 09:26:23 -0300

    Mais tailwind

--- vixz/src/components/about/about.js
+++ vixz/src/components/about/about.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 export const About = () => {
   return (
-      <div className="pt-4 flex flex-row justify-center bg-slate-500 flex-1">
+      <div className="pt-4 flex flex-row justify-center px-2">
         <div className="">
           <div className="">
             <div className="">
               <h3>Sobre</h3>
               <span className="line"></span>
-              <p className='pb-2 text-slate-500'>Um aplicativo de busca de vagas de emprego e cursos. Onde você expõe seu
+              <p className='pb-2'>Um aplicativo de busca de vagas de emprego e cursos. Onde você expõe seu
                 currículo para as empresas, e ainda com cursos disponíveis para melhorar suas chances de
                 contratação!</p>
             </div>
             <div className="">
-              <div className="flex flex-col md:flex-row justify-between gap-1">
+              <div className="flex flex-col md:flex-row items-center md:items-baseline gap-1">
                 <div className="w-1/3">
                   <div className="icon-box wow fadeInUp"> <i className="fa fa-life-ring"
                       aria-hidden="true"></i>
