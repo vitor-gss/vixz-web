c1a9cc8424098579939529235245866fac81916b
Vitor Gabriel da Silva Santos <126255280+vitor-gss@users.noreply.github.com>
2024-12-05 11:14:08 -0300

    push

--- vixz/src/components/initial/initial.js
+++ vixz/src/components/initial/initial.js
@@ -2,31 +2,29 @@ import React from 'react'
 
 export const Initial = () => {
   return (
-<section class="banner" data-scroll-index='0'>
-      <div class="banner-overlay">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8 col-sm-12">
-              <div class="banner-text">
-                <h2 class="white">Vixz</h2>
-                <h6 class="white"><strong>Emprego e estudos</strong> em um só lugar.</h6>
-                <p class="banner-text white">Em busca da oportunidade dos seus sonhos? O Vixz é o seu
+<section className="banner" >
+      <div className="">
+        <div className="px-5">
+          <div className="flex flex-col md:flex-row items-center">
+              <div className="banner-text md:text-start">
+                <h2 className="white text-4xl md:text-6xl">Vixz</h2>
+                <h6 className="white text-xl"><strong>Emprego e estudos</strong> em um só lugar.</h6>
+                <p className="banner-text white text-lg md:w-3/5">Em busca da oportunidade dos seus sonhos? O Vixz é o seu
                   parceiro confiável
                   na jornada profissional, oferecendo uma experiência excepcional para impulsionar sua
                   carreira para novos patamares!</p>
-                  <ul class="">
-                    <li class="nav-item wow fadeInUp"> <a class="nav-link entrar" href="login.html">Entrar</a> </li>
-                    <li class="nav-item wow fadeInUp"> <a class="nav-link cadastrar" href="cadastro.html">Cadastrar</a> </li>
+                  <ul className="">
+                    <li className="nav-item wow fadeInUp"> <a className="nav-link entrar" href="login.html">Entrar</a> </li>
+                    <li className="nav-item wow fadeInUp"> <a className="nav-link cadastrar" href="cadastro.html">Cadastrar</a> </li>
                   </ul>
                 <ul>
-                  <li><a href="#"><img src="./images/appstore.png" class="wow fadeInUp"
+                  <li><a href="#"><img src="./images/appstore.png" className="wow fadeInUp"
                         data-wow-delay="0.4s" /></a></li>
-                  <li><a href="#"><img src="./images/playstore.png" class="wow fadeInUp"
+                  <li><a href="#"><img src="./images/playstore.png" className="wow fadeInUp"
                         data-wow-delay="0.7s" /></a></li>
                 </ul>
               </div>
-            </div>
-            <div class="col-md-4 col-sm-12"> <img src="./images/Fórum.png" class="img-fluid wow fadeInUp" />
+            <div className="flex justify-center h-"> <img src="./images/Fórum.png"/>
             </div>
           </div>
         </div>
