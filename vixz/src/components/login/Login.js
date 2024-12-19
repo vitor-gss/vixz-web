b3bdd61648367f9acdf60e7fd8466f1548bc783c
Vitor Gabriel da Silva Santos <126255280+vitor-gss@users.noreply.github.com>
2024-12-19 12:09:22 -0300

    Mudança de estilos

--- vixz/src/components/login/Login.js
+++ vixz/src/components/login/Login.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-        <h1>Telinha de login</h1>
+    <div className='p-5'>
+        <h1 className='text-6xl bg-red-500'>R</h1>
+        <h1 className='text-6xl bg-orange-500'>A</h1>
+        <h1 className='text-6xl bg-yellow-500'>I</h1>
+        <h1 className='text-6xl bg-green-500'>N</h1>
+        <h1 className='text-6xl bg-cyan-500'>B</h1>
+        <h1 className='text-6xl bg-blue-500'>O</h1>
+        <h1 className='text-6xl bg-purple-500'>W</h1>
     </div>
   )
 }
--- vixz/src/components/paginaInicial/components/initial/Initial.js
+++ vixz/src/components/paginaInicial/components/initial/Initial.js
@@ -1,35 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const Initial = () => {
   return (
-<section className="banner" >
-      <div className="">
-        <div className="px-5">
+<section className="bg-purple-900" >
+        <div className="p-5">
           <div className="flex flex-col md:flex-row items-center">
-              <div className="banner-text md:text-start">
-                <h2 className="white text-4xl md:text-6xl">Vixz</h2>
-                <h6 className="white text-xl"><strong>Emprego e estudos</strong> em um só lugar.</h6>
-                <p className="banner-text white text-lg md:w-3/5">Em busca da oportunidade dos seus sonhos? O Vixz é o seu
+              <div className="text-white md:text-start">
+                <h2 className="text-4xl md:text-6xl">Vixz</h2>
+                <h6 className="text-xl"><strong>Emprego e estudos</strong> em um só lugar.</h6>
+                <p className="text-lg md:w-3/5">Em busca da oportunidade dos seus sonhos? O Vixz é o seu
                   parceiro confiável
                   na jornada profissional, oferecendo uma experiência excepcional para impulsionar sua
                   carreira para novos patamares!</p>
-                  <ul className="">
-                    <li className="nav-item wow fadeInUpx"> <a className="nav-link entrar" href="login.html">Entrar</a> </li>
-                    <li className="nav-item wow fadeInUp"> <a className="nav-link cadastrar" href="cadastro.html">Cadastrar</a> </li>
-                  </ul>
-                <ul>
-                  <li><a href="#"><img src="./images/appstore.png" className="wow fadeInUp"
-                        data-wow-delay="0.4s" /></a></li>
-                  <li><a href="#"><img src="./images/playstore.png" className="wow fadeInUp"
-                        data-wow-delay="0.7s" /></a></li>
-                </ul>
+                  <Link className='bg-purple-200 text-black rounded p-5 text-center' to="/login">Entrar</Link>
               </div>
-            <div className="flex justify-center h-"> <img src="./components/paginaInicial/images/Fórum.png"/>
-            </div>
+            <div className="flex justify-center h-"> <img src="./images/Fórum.png"/></div>
           </div>
         </div>
-      </div>
-        {/* <span class="svg-wave"> <img class="svg-hero" src="images/applight-wave.svg"> </span> */}
     </section>
   )
 }
